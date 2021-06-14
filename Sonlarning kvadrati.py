d865728e4d8ba5a3d1f3cbc438e0f63e05685c9d
Adhamjon1 <85832612+Adhamjon1@users.noreply.github.com>
2021-06-14 10:04:59 +0500

    Add files via upload

---
+++ Sonlarning kvadrati.py
@@ -0,0 +1,3 @@
+sonlar = list(range(1,100))
+for son in sonlar:
+    print(f"{son} ning kvadrati {son**2}ga teng")
---
+++ Yoshingizni aniqlang.py
@@ -0,0 +1,3 @@
+t_yil = int(input("Tug'ilgan yilingizni kiriting : "))
+yosh = 2021 - t_yil
+print ("Siz" , yosh ,"yoshda ekansiz")
